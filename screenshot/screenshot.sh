da5bab11e5ec2eacd38c9b928624265bd8828320
dps <dps141618@gmail.com>
2020-05-01 18:29:13 +0100

    update
    now uses flameshot instead of gnome-screenshot (it seems to not work well with wayland), updated url to copy diamonds fork (discord embed support)

--- screenshot/screenshot.sh
+++ screenshot/screenshot.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
-areass() { 
-    gnome-screenshot -a -c && xclip -selection clipboard -t image/png -o > /tmp/image.png
-    curl -F'file=@/tmp/image.png' https://0x0.arikawa-hi.me | xclip -selection clipboard
+areass() {
+	# Copy image to clipboard and send it to /tmp/image.png
+    flameshot gui -r && xclip -selection c -t image/png -o > /tmp/image.png
+
+	# Send image to 0x0.st server
+    curl -F'file=@/tmp/image.png' https://0x0.st | xclip -selection c
+
+	# When image is in the clipboard, remove everything except after the /
+	# then add everything after the / into a variable
+	# then add it at the end of new url
+	COPY_IMG="xclip -selection c -o | cut -d / -f 4"
+	PNG=$(eval $COPY_IMG)
+	echo "https://0x0.arikawa-hi.me/$PNG" | xclip -selection c
+
 }
 areass
