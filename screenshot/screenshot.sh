310bf2b164c2db27d6ff08efdf076fb54294f59b
david <42849369+dps910@users.noreply.github.com>
2021-01-07 10:00:28 +0000

    send images to your domain

--- screenshot/screenshot.sh
+++ screenshot/screenshot.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 areass() {
-	# Copy image to clipboard and send it to /tmp/image.png
-    flameshot gui -r && xclip -selection c -t image/png -o > /tmp/image.png
-
-	# Send image to 0x0.st server
-    curl -F'file=@/tmp/image.png' https://0x0.st | xclip -selection c
-
-	# When image is in the clipboard, remove everything except after the /
-	# then add everything after the / into a variable
-	# then add it at the end of new url
-	COPY_IMG="xclip -selection c -o | cut -d / -f 4"
-	PNG=$(eval $COPY_IMG)
-	echo "https://0x0.arikawa-hi.me/$PNG" | xclip -selection c
+  RANDOM="$(openssl rand -hex 2)"
+  IMG="$RANDOM.png"
+  maim -s --format=png /tmp/${IMG} | xclip -selection clipboard -t image/png
+  sshpass -p "password" scp /tmp/${IMG} root@ipaddress:/var/domain # replace this with where you upload your images
+  echo -n "https://domain/${IMG}" | xclip -selection c
 }
 areass
